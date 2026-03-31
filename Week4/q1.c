ac75c7113daa946c70ad0be4935060c96e8d3f37
charlie6713 <charliecui67@gmail.com>
2026-03-31 23:27:08 +1100

    Week4

--- Week4/q1.c
+++ Week4/q1.c
@@ -1,1 +1,8 @@
-ss
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    unsigned char x = 0b1010;
+    unsigned int result = (x >> 3) & 0b1111;
+    printf("the left most bit is %u\n", result);
+    return 0;
+}
