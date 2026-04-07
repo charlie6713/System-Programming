cc42b789b9e787c568f2b462754b64910a472cb4
charlie6713 <charliecui67@gmail.com>
2026-04-07 23:44:39 +1000

    week4 day5

--- Week4/q5.c
+++ Week4/q5.c
@@ -1,2 +1,15 @@
+#include <stdio.h>
 
+int main(void){
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            c = c - ('a' - 'A');
+        }
+        putchar(c);
+    }
+
+    return 0;
+}
 
